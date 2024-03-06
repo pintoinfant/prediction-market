86869384fab09d0f8abad0e94eec4613be9a131c
pintoinfant <pintoinfant5650@gmail.com>
2024-03-06 18:36:29 +0530

    initaited repository

---
+++ blockend/secret-network/installer.sh
@@ -0,0 +1,11 @@
+
+sudo apt-get install git make
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup target add wasm32-unknown-unknown
+cargo install cargo-generate --features vendored-openssl
+make build
+cd node
+npm install
+node index.js
+node create_keys.js
+node get_keys.js
